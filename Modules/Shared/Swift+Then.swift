255cf59aaa6469701373f5e1a7460b09f2286e9c
Grigory Entin <grigory.entin@gmail.com>
2018-05-26 15:58:29 +0200

    Segregated XXX+Then.

---
+++ Modules/Shared/Swift+Then.swift
@@ -0,0 +1,11 @@
+//
+//  Swift+Then.swift
+//  WeatherAppKit
+//
+//  Created by Grigory Entin on 26/05/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Then
+
+extension Array : Then {}
