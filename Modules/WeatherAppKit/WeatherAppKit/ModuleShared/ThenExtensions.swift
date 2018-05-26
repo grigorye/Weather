c6ced95504bec600cdadf7e69c5e702b4e2c4342
Grigory Entin <grigory.entin@gmail.com>
2018-05-26 15:59:49 +0200

    Segregated ThenExtensions.

---
+++ Modules/WeatherAppKit/WeatherAppKit/ModuleShared/ThenExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  ThenExtensions.swift
+//  WeatherAppKit
+//
+//  Created by Grigory Entin on 26/05/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Then
+
+extension UserCityInfoAndLastWeatherInfo : Then {}
+extension LastWeatherInfo : Then {}
+extension UserCityInfo : Then {}
