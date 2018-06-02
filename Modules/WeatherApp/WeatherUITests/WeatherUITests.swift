fae45f7148b4941dc6ca59f6979b63a426bb78be
Grigory Entin <grigory.entin@gmail.com>
2018-06-02 14:16:36 +0200

    Added test for repeated navigation.

--- Modules/WeatherApp/WeatherUITests/WeatherUITests.swift
+++ Modules/WeatherApp/WeatherUITests/WeatherUITests.swift
@@ -8,15 +8,37 @@
 
 import XCTest
 
+let app = XCUIApplication()
+
 class WeatherUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
         
         continueAfterFailure = false
-        XCUIApplication().launch()
+        app.launchArguments = [
+            "-forceDebugScene", "YES"
+        ]
+        app.launchEnvironment = [
+            "MallocStackLogging":"1"
+        ]
+        app.launch()
     }
     
-    func test() {
+    func testRepeatedNavigation() {
+        
+        let app = XCUIApplication()
+        
+        let debugButton = app.navigationBars["WeatherAppKit.UserCitiesView"].buttons["Debug"]
+        let mainStaticText = app.tables.staticTexts["Main"]
+        let addCityButton = app.tables.buttons["Add City"]
+        let cancelButton = app.buttons["Cancel"]
+
+        for _ in 0..<50 {
+            mainStaticText.tap()
+            addCityButton.tap()
+            cancelButton.tap()
+            debugButton.tap()
+        }
     }
 }
